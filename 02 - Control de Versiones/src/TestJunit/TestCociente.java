744c68f7fedc95c46b9c487eb4247b8dc7e8c4e3
Adrian Caballero <92646371+Schlaufuchs17@users.noreply.github.com>
2022-02-27 20:16:02 +0100

    Eliminados errores en la ejecucion
    No funcionaba bien con la raiz, cambiado el nombre, por eso no funcionaba

--- 02 - Control de Versiones/src/TestJunit/TestCociente.java
+++ 02 - Control de Versiones/src/TestJunit/TestCociente.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestCociente {
-Cociente c1 ; //ACF: Creamos el tipo cociente con el que haremos las pruebas
+Cociente C_1 ; //ACF: Creamos el tipo cociente con el que haremos las pruebas
 	
 	@BeforeEach 
 	public  void clean () {
-		c1 = new Cociente(); 
+		C_1 = new Cociente(); 
 	}
 
 	@Test
@@ -20,18 +20,18 @@ Cociente c1 ; //ACF: Creamos el tipo cociente con el que haremos las pruebas
 		
 		
 		double Esperado = 7.62;
-		double Obtenido = c1.divisionReales(70.1, 9.2);
+		double Obtenido = C_1.divisionReales(70.1, 9.2);
 		assertEquals(Esperado, Obtenido);
 		
 		
 		
 		Esperado =9.223372036854776E16;
-		Obtenido = c1.divisionReales(70.2, 0);
+		Obtenido = C_1.divisionReales(70.2, 0);
 		assertEquals(Esperado, Obtenido);
 		
 		
 		Esperado =0.0;
-		Obtenido = c1.divisionReales(0, -9870.5);
+		Obtenido = C_1.divisionReales(0, -9870.5);
 		assertEquals(Esperado, Obtenido);
 		
 	}
@@ -41,13 +41,13 @@ Cociente c1 ; //ACF: Creamos el tipo cociente con el que haremos las pruebas
 	
 		
 		int Esperado = 20;
-		double Obtenido = c1.divisionEnteros(80, 4);
+		double Obtenido = C_1.divisionEnteros(80, 4);
 		assertEquals(Esperado, Obtenido);
 		
 	
 		
 		Esperado= 0;
-		Obtenido = c1.divisionEnteros(0, 140);
+		Obtenido = C_1.divisionEnteros(0, 140);
 		assertEquals(Esperado, Obtenido);
 		
 	}
@@ -56,23 +56,23 @@ Cociente c1 ; //ACF: Creamos el tipo cociente con el que haremos las pruebas
 	public void testInverso() {
 		
 		double Esperado = 0.02;
-		double Obtenido = c1.inverso(50.4);
-		double inverso = Math.pow(c1.inverso(50.4), -1);
+		double Obtenido = C_1.inverso(50.4);
+		double inverso = Math.pow(C_1.inverso(50.4), -1);
 		
 		assertEquals(Esperado, Obtenido);
 		
-		if (c1.inverso(50.4)*inverso == 1) {
+		if (C_1.inverso(50.4)*inverso == 1) {
 			assertTrue(true);
 		}
 		
 		
 		Esperado = 9.223372036854776E16;
-		Obtenido = c1.inverso(0);
-		inverso = Math.pow(c1.inverso(0), -1);
+		Obtenido = C_1.inverso(0);
+		inverso = Math.pow(C_1.inverso(0), -1);
 		
 		assertEquals(Esperado, Obtenido);
 		
-		if (c1.inverso(0)*inverso == 1) {
+		if (C_1.inverso(0)*inverso == 1) {
 			assertTrue(true);
 		}
 		
@@ -82,30 +82,30 @@ Cociente c1 ; //ACF: Creamos el tipo cociente con el que haremos las pruebas
 		
 		
 		double Esperado = 8.37;
-		double Obtenido = c1.raiz(70);
+		double Obtenido = C_1.raiz(70);
 				
 		assertEquals(Esperado, Obtenido);
 		
-		if (Obtenido*Obtenido == c1.raiz(70)) {
+		if (Obtenido*Obtenido == C_1.raiz(70)) {
 			assertTrue(true);
 		}
 		
 		
 		Esperado = 0.0;
-		Obtenido = Math.round(Math.sqrt(c1.raiz(0.0))*100.0)/100.0;
+		Obtenido = Math.round(Math.sqrt(C_1.raiz(0.0))*100.0)/100.0;
 				
 		assertEquals(Esperado, Obtenido);
 		
-		if (Obtenido*Obtenido == c1.raiz(0.0)) {
+		if (Obtenido*Obtenido == C_1.raiz(0.0)) {
 			assertTrue(true);
 		}
 		
 		Esperado = 0.0;
-		Obtenido = Math.round(Math.sqrt(c1.raiz(-7))*100.0)/100.0;
+		Obtenido = Math.round(Math.sqrt(C_1.raiz(-7))*100.0)/100.0;
 				
 		assertEquals(Esperado, Obtenido);
 		
-		if (Obtenido*Obtenido == c1.raiz(-7)) {
+		if (Obtenido*Obtenido == C_1.raiz(-7)) {
 			assertTrue(true);
 		}
 
