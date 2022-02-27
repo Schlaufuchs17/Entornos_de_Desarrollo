1c934500ff7e85645adcbb8cfb270e92a20a95df
Adrian Caballero <92646371+Schlaufuchs17@users.noreply.github.com>
2022-02-27 20:25:42 +0100

    Creado la linea package
    Creado la linea package para poder importar los archivos junit
    
    package AE3;

--- 02 - Control de Versiones/src/Main.java
+++ 02 - Control de Versiones/src/Main.java
@@ -1,3 +1,5 @@
+package AE3;
+
 import Objetos.Suma;
 import Objetos.Resta;
 import Objetos.Producto;
@@ -15,20 +17,20 @@ public class Main {
 		System.out.println("Resultado de la suma");
 		System.out.println("Suma de dos numeros reales: "+suma.sumareal(2.5,2.5));
 		System.out.println("Suma de dos numeros enteros: "+suma.sumaint(7,7));
-		System.out.println("Suma de tres números reales: "+suma.sumareal(2.5,2.5,3.7));
+		System.out.println("Suma de tres nÃºmeros reales: "+suma.sumareal(2.5,2.5,3.7));
 		System.out.println("Resultado del valor acumulado: "+suma.ValorAcumulado(5));
 			
 		System.out.println("Resultados de la resta");
-		System.out.println("Resta de dos números reales: "+resta.resultado(4.5,8.5));
-		System.out.println("Resta de dos números enteros: "+resta.resultado(12,4));
-		System.out.println("Resta de tres números reales: "+resta.resultado(1.2,8.7,4.4));
+		System.out.println("Resta de dos nÃºmeros reales: "+resta.resultado(4.5,8.5));
+		System.out.println("Resta de dos nÃºmeros enteros: "+resta.resultado(12,4));
+		System.out.println("Resta de tres nÃºmeros reales: "+resta.resultado(1.2,8.7,4.4));
 		System.out.println("Resultado del valor acumulado: "+resta.resultado(15));
 		
 		System.out.println("Resultados de la clase producto");
-		System.out.println("Multiplicación de dos números enteros: "+producto.productoint(2, 5));
-		System.out.println("Multiplicación de dos números reales: "+producto.producto(3.5, 5.5));
-		System.out.println("Multiplicación de tres números reales: "+producto.producto(5.5, 8.5, 9.5));
-		System.out.println("Cálculo de la potencia: "+producto.potencia(5, 5));
+		System.out.println("MultiplicaciÃ³n de dos nÃºmeros enteros: "+producto.productoint(2, 5));
+		System.out.println("MultiplicaciÃ³n de dos nÃºmeros reales: "+producto.producto(3.5, 5.5));
+		System.out.println("MultiplicaciÃ³n de tres nÃºmeros reales: "+producto.producto(5.5, 8.5, 9.5));
+		System.out.println("CÃ¡lculo de la potencia: "+producto.potencia(5, 5));
 
 		System.out.println("Resultados de la clase cociente");
         System.out.println("El valor de la division es "+cociente.cociente(10,2));
